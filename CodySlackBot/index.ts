c6abc9420cc6c33f0ca8350b1c283f234258d422
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-12-11 16:50:03 +0100

    fix call

--- CodySlackBot/index.ts
+++ CodySlackBot/index.ts
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const generatePrompt = (animal: string): string => {
   const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+    animal.toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
   
   Animal: Cat
@@ -15,7 +15,7 @@ const generatePrompt = (animal: string): string => {
 };
 
 /*
-Call with: { animal: "panda" }
+Call with: { "animal": "panda" }
 */
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -27,14 +27,35 @@ const httpTrigger: AzureFunction = async function (
   //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
   //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+  const animal = req.query.animal ?? req.body.animal;
+
+  if (!animal) {
+    context.res = {
+      status: 400,
+      body: 'Missing param "animal"',
+    };
+    return;
+  }
+
+  const { OPENAI_API_KEY } = process.env;
+  context.log("OPENAI_API_KEY=", OPENAI_API_KEY);
+
+  if (!OPENAI_API_KEY) {
+    context.res = {
+      status: 400,
+      body: 'Missing env OPENAI_API_KEY',
+    };
+    return;
+  }
+
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: generatePrompt(req.query.animal),
+    prompt: generatePrompt(req.query.animal ?? req.body.animal),
     temperature: 0.6,
   });
 
--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-
 ## Developing
 
+- Configure a .env file in the root of the project with values for:
+  - `OPENAI_API_KEY`
+  - This file is loaded by the `envFile` prop in `.vscode/launch.json`
+- In VS Code, run CodySlackBot/index.ts with F5
+- `curl http://localhost:7071/api/CodySlackBot\?animal\=panda`
 
-* Configure a .env file in the root of the project with values for:
-    * `OPENAI_API_KEY`
-* In VS Code, run CodySlackBot/index.ts with F5
-* In VS Code Azure plugin, under Workspace, Function CodySlackBot, "Run function now"
+or instead of the last line:
+
+- In VS Code Azure plugin, under Workspace, Function CodySlackBot, "Run function now"
+- Use request body: `{ "animal": "panda" }`
+
+## Deploy to Prod Slot
+
+* set envars in Azure Function App console under > configuration > Application settings, add:
+
+```
+OPENAI_API_KEY=the_api_key
+```
+
+* In VS Code, in the Azure toolbar under Resources, expand codestar-website-api > Slots > test and right-click. Click "Swap slot..."
+* Select the production slot
+
+or:
+
+* in VS Code, in the Azure toolbar under Workspace > click the "deploy" icon (cloud with up arrow), select the correct Resource Group and the codestar-cody-slackbot Function App. Allow overwriting the existing deployment.
+
+Test with `curl https://???/api/CodySlackBot/\?animal\=panda`
